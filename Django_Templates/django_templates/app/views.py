39b72ea07af1856c463066aa02bf20b1ebfd467e
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2021-12-10 17:27:40 -0300

    new commit

--- Django_Templates/django_templates/app/views.py
+++ Django_Templates/django_templates/app/views.py
@@ -25,7 +25,15 @@ def index(request):
 
 
 def sobre(request):
-    return render(request, 'empresa/about.html')
+    nome_da_empresa = "CLINICA PET"
+    descricao_da_empresa = "A nossa rede de clínicas está no mercado a mais de 20 anos. São mais de 50 profissionais trabalhando conosco."
+
+    contato_empresa = {
+        "endereco": "Avenida Brigadeiro Faria Lima, 1987 - Itaim Bibi - São Paulo - SP",
+        "telefone": "+55 11 97674-1514",
+        "email": "clinicapet@clinicasp.com.br",
+    }
+    return render(request, 'empresa/about.html', {'nome_da_empresa': nome_da_empresa, 'descricao_da_empresa': descricao_da_empresa, 'contato_empresa': contato_empresa})
 
 
 def servicos(request):
